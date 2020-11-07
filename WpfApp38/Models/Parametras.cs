9bb4d379b9678b9c92efa2f2c0f19f5ef7c1353b
MrDotGamer <40805382+MrDotGamer@users.noreply.github.com>
2020-11-07 23:02:13 +0200

    Create Parametras.cs

---
+++ WpfApp38/Models/Parametras.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp38.Models
+{
+    public class Parametras
+    {
+        public string Id { get; set; }
+        public string Value { get; set; }
+    }
+}
