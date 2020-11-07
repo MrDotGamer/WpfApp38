5a0f39c91dc0b8280e7e8aaf2d7849e302a53bca
MrDotGamer <40805382+MrDotGamer@users.noreply.github.com>
2020-11-08 00:17:39 +0200

    createConfigFiles
    created config files by example

--- WpfApp38/Models/Parametras.cs
+++ WpfApp38/Models/Parametras.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace WpfApp38.Models
@@ -8,5 +9,16 @@ namespace WpfApp38.Models
     {
         public string Id { get; set; }
         public string Value { get; set; }
+        public static void WriteParametersToFile(List<Parametras> parameterList,string toFile)
+        {
+            StringBuilder path = new StringBuilder(Const.FileFolder);
+            path.Append(toFile);
+            StringBuilder parameters = new StringBuilder();
+            foreach (var parametras in parameterList)
+            {
+                parameters.Append($"{parametras.Id}:{parametras.Value};");
+            }
+            File.WriteAllText(path.ToString(), parameters.ToString());
+        }
     }
 }
